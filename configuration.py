42f66fc42efef3b7c3e4db6d0bab0aae13fc6ba7
Megan Mazyrevskaya <meganlimon5@gmail.com>
2026-06-20 22:17:38 +0300

    Update API autotest

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://ee4b38e7-ae7a-4258-aa56-327db29b5c83.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://df0a8886-1159-494a-a1fc-b1b19009fa8b.serverhub.praktikum-services.ru"
 CREATE_ORDERS = "/api/v1/orders"
 GET_ORDER_BY_TRACK = "/api/v1/orders/track"
--- get_order_by_track_number_test.py
+++ get_order_by_track_number_test.py
@@ -4,10 +4,10 @@ import data
 def get_new_order_track():
     response = sender_stand_request.post_new_order(data.order_body)
     return response.json()["track"]
-
+# Мэган Мазуревская, 43-я когорта - финальный проект. Инженер по тестированию плюс
 def test_get_order_by_track_number():
     order_response = sender_stand_request.post_new_order(data.order_body)
     track = order_response.json()["track"]
     response = sender_stand_request.get_order_by_track(track)
-   
+    print(response.status_code)
     assert response.status_code == 200 
