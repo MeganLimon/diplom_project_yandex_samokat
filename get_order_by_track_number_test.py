4a0e3485b7e394ff9eae142eeaed8003c96a11c4
Megan Mazyrevskaya <meganlimon5@gmail.com>
2026-06-20 23:03:38 +0300

    My final project 3

--- get_order_by_track_number_test.py
+++ get_order_by_track_number_test.py
@@ -2,6 +2,7 @@ import sender_stand_request
 import data
 
 # Мэган Мазуревская, 43-я когорта - финальный проект. Инженер по тестированию плюс
+# Проверяет, что после создания заказа его можно получить по треку
 def test_get_order_by_track_number():
     order_response = sender_stand_request.post_new_order(data.order_body)
     track = order_response.json()["track"]
